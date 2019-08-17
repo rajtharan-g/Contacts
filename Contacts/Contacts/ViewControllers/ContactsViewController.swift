e47144822618edf6328895f3b8d67879bc2c9b6d
Rajtharan G <rajtharan@infivention.com>
2019-08-17 11:31:46 +0530

    - Folder restructured - Added asset images

--- Contacts/Contacts/ViewControllers/ContactsViewController.swift
+++ Contacts/Contacts/ViewControllers/ContactsViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactsViewController: UIViewController {
 
+    // MARK: - View life cycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
