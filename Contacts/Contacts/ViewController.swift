4476831dfe3dedefd4956ba40f28b04bbb779a40
Rajtharan G <rajtharan@infivention.com>
2019-08-17 11:04:27 +0530

    - New project created

---
+++ Contacts/Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Rajtharan G on 17/08/19.
+//  Copyright © 2019 Rajtharan G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
